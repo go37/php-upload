4b6b5a6105f946ea79f1978b229f9772add279dc
Olivier Gourdonneau <olivier.gourdonneau@meteo.fr>
2021-11-11 12:49:06 +0100

    second commit cith corrections

--- form.php
+++ form.php
@@ -1,21 +1,28 @@
 <?php
-
-// Je vérifie que le formulaire est soumis, comme pour tout traitement de formulaire.
-
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
-    // chemin vers un dossier sur le serveur qui va recevoir les fichiers transférés (attention ce dossier doit être accessible en écriture)
-
+    // configuration générale
     $uploadDir = 'uploads/';
+    $authorizedExtensions = ['jpg', 'png', 'gif', 'webp'];
+    $maxFileSize = 1000000;
 
-    // le nom de fichier sur le serveur est celui du nom d'origine du fichier sur le poste du client (mais d'autre stratégies de nommage sont possibles)
-
-    $uploadFile = $uploadDir . basename($_FILES['avatar']['name']);
-
-
-    // on déplace le fichier temporaire vers le nouvel emplacement sur le serveur. Ca y est, le fichier est uploadé
-
+    // gestion de l'upload
+    $extension = pathinfo($_FILES['avatar']['name'], PATHINFO_EXTENSION);
+    $uploadFile = $uploadDir . uniqid("", false) . "." . $extension;
     move_uploaded_file($_FILES['avatar']['tmp_name'], $uploadFile);
+
+    // infos profil
+    $firstname = $_POST["firstname"];
+    $lastname = $_POST["lastname"];
+    $age = $_POST["age"];
+
+    if (!in_array($extension, $authorizedExtensions)) {
+        $errors[] = 'Veuillez sélectionner une image de type jpg, jpeg, png ou webp !';
+    }
+
+    if (file_exists($_FILES['avatar']['tmp_name']) && filesize($_FILES['avatar']['tmp_name']) > $maxFileSize) {
+        $errors[] = "Votre fichier doit faire moins de 1Mo !";
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -29,10 +36,28 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 </head>
 
 <body>
-    <form action="uploaded.php" method="post" enctype="multipart/form-data">
-        <label for="imageUpload">Upload an profile image</label>
+    <?php
+    if (!empty($_FILES)) {
+        echo "<img src='$uploadFile' alt='image téléchargée' /><br>";
+        echo "Prénom : $firstname<br>Nom : $lastname<br>Age : $age ans  <br><br>";
+    }
+    ?>
+
+    <form method="post" enctype="multipart/form-data">
+
+        <label for="firstname">Prénom :</label><br>
+        <input type="text" id="firstname" name="firstname" required><br>
+
+        <label for="lastname">Nom :</label><br>
+        <input type="text" id="lastname" name="lastname" required><br>
+
+        <label for="age">Age :</label><br>
+        <input type="number" id="age" name="age" required><br>
+
+        <label for="imageUpload">Télécharger votre avatar</label>
         <input type="file" name="avatar" id="imageUpload" />
-        <button name="send">Send</button>
+
+        <button name="send">Envoyer</button>
     </form>
 </body>
 
